966c857e7ef2311ac2640a56ba23c6164b5e9c5f
Dominik Schöni <ds@clubzone.ch>
2012-10-29 13:57:47 +0100

    Bootstrapped InvestmentGame

--- library/ibftool/Form/InvestmentGame.php
+++ library/ibftool/Form/InvestmentGame.php
@@ -15,9 +15,13 @@ class ibftool_Form_InvestmentGame extends Zend_Form {
 		$amount = new ibftool_Form_Element_Note("amount");
 		$amount->setRequired(false);
 		$amount->setLabel("Investment: ");
-		$amount->setValue("<span id='amount' class='amount'>Anteil bitte wählen</span><span id='amounteuro'></span>");
+		$amount->addValidator(new Zend_Validate_Between(0, 100));
+		$amount->addValidator(new Zend_Validate_Digits());
+		$amount->addErrorMessage("Bitte tragen sie eine Zahl von 0 bis 100 ein.");
+		$amount->addDecorator(new ibftool_Form_Decorators_Currency(array("currency" => "Rappen")));
 		$this->addElement($amount);
 		
+		/*
 		$slider = new ZendX_JQuery_Form_Element_Slider("investment");
 		$slider->setRequired(true);
 		$slider->setLabel("<span class='investment-label-null'>0%</span><span class='investment-label-hundred'>100%</span>");
@@ -36,10 +40,34 @@ class ibftool_Form_InvestmentGame extends Zend_Form {
 		));
 		
 		$slider->addErrorMessage("Der Wert muss zwischen 0 und 100 liegen und eine ganze Zahl sein.");
+		*/
+		
+		$slider = new ZendX_JQuery_Form_Element_Slider("investment");
+		$slider->setRequired(true);
+		$slider->setLabel("<span style=''>0 Rappen</span><span style='float: right'>100 Rappen</span>");
+		$slider->addDecorator("Label" ,array("escape"=>false, 'tag' => 'dt'));
+		$slider->setJQueryParams(array(
+		  'min' => 0,
+		  'max' => 100,
+		  'value' => 0,
+		  'step' => 1,
+		  'slide' => new Zend_Json_Expr("function(event, ui) {
+		   	document.getElementById('amount').value = ui.value;
+		   	$('.ui-slider-handle').show();
+		   	document.getElementById('Weiter').style.display = '';	
+		   }"),
+			'create' => new Zend_Json_Expr("function(event, ui) {
+			if (document.getElementById('amount').disabled) {
+		  		$('#investment-slider').slider( 'option', 'disabled', true );
+		  	}
+			}") 
+		));
+		
 		$this->addElement($slider);
 		
 		$submit = new Zend_Form_Element_Submit("Weiter");
 		$submit->setIgnore(true);
+		$submit->setAttrib("class", "btn btn-primary");
 		$submit->removeDecorator('DtDdWrapper');
 		
 		$this->addElement($submit);
